bb063c59b23bb81ada8d02de823f2933d980ed3b
carlos-alfonzo <casr@tutanota.com>
2025-08-07 18:18:59 -0600

    Added multipage modality and report directory

--- pages/modalidad40.py
+++ pages/modalidad40.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.title("Modalidad 40")
 
 st.sidebar.radio("Salario Asignado", ('1000', '2000'))
+
+fecha_inicio = st.date_input("Fecha de inicio")
+meses_mod40 = st.number_input("Meses de modalidad 40", min_value=1, max_value=12, value=1)
+salario_asignado = st.number_input("Salario Asignado", min_value=1000, max_value=2000, value=1000)
