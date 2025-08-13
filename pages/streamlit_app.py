c4e1fddc5a4036d9f42dc7d056872d519a3d0921
Alfonzo1114 <casr@tutanota.com>
2025-08-12 18:25:33 -0600

    Added new page for general information UI display.

--- pages/streamlit_app.py
+++ pages/streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 pg = st.navigation([
     st.Page("CalculoManual.py"),
     st.Page("DatosGenerales.py"),
+    st.Page("DatosGenerales_Mejorado.py"),
 
     st.Page("modalidad40.py")
 ])
