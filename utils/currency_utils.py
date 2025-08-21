910f35ce6422fff45620831a5a835cff283449b2
Alfonzo1114 <casr@tutanota.com>
2025-08-20 22:30:29 -0600

    Fixed function aguinaldos in Pensiones_V3.ipynb Added function tabla_aguinaldos to calculations.py Implemented function tabla_aguinaldos to Changed year input in CalculoManual.py to fecha_pension Fixed cuantia vejez format in calculations.py Added aguinaldos function correctly attached to input values in CalculoManual.py

--- utils/currency_utils.py
+++ utils/currency_utils.py
@@ -2,7 +2,6 @@ def convert_double_currency(value):
     # We will receive a float value and return a string with $ and 2 decimals
     return "${:,.2f}".format(value)
 
-# Example usage:
-value = 1234.56
-formatted_value = convert_double_currency(value)
-print(formatted_value)
+def convert_string_currency(value):
+    # We will receive a string value and return a float value
+    return float(value.replace("$", "").replace(",", ""))
